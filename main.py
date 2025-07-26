e029a503bdf5034392e39d947efb730a9346a65e
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2025-07-26 12:41:50 +0600

    added embedding model and vector store

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import fitz
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+import os
 
 def lazy_load_pdf_pages(file_path):
     doc = fitz.open(file_path)
@@ -34,3 +37,28 @@ print("len of texts", len(texts))
 print("len of docs", len(documents))
 print(documents[0].page_content)
 print(documents[0].metadata)
+
+# Embedding model for both Bangla and English
+embedding = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-base")
+
+# Vector store using Chroma
+
+db_loc = './bn_chroma_db_mltlng'
+add_documents = not os.path.exists(db_loc)
+
+ids = None
+if add_documents:
+    ids = [str(i) for i in range(len(documents))]
+
+vect_store = Chroma(
+    collection_name="my_collection",
+    persist_directory=db_loc,
+    embedding_function=embedding
+)
+
+if add_documents:
+    # print("inside if")
+    vect_store.add_documents(documents=documents, ids=ids)
+
+stored = vect_store.get(include=["documents"])
+print("Stored docs:", len(stored["documents"]))
